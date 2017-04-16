2182073bfb59848f04f6757e5bd9c70781388088
nevver <io.nevver@yahoo.com>
2017-04-16 15:13:36 -0700

    Public key publish @ signup

--- EIMv1/src/EIMv1/Controllers/AuthController.cs
+++ EIMv1/src/EIMv1/Controllers/AuthController.cs
@@ -8,12 +8,25 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Flurl;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
 
 namespace EIMv1.Controllers.Web
 {
     public class AuthController : Controller
     {
-      
+        private static string chronoskeysUrl = "https://chronoskeys.co/";
+
+        public class KeyPost
+        {
+            [JsonProperty("user")]
+            public string user { get; set; }
+
+            [JsonProperty("key")]
+            public string key { get; set; }
+        }
+
         public IActionResult Signup()
         {
             //ADD Todo: if user been authenticated, redirect to messaging page
@@ -24,35 +37,164 @@ namespace EIMv1.Controllers.Web
             return View();
         }
 
+        [HttpGet]
+        public async Task<bool> CheckUsernameAvailability(SignupViewModel model)
+        {
+            try
+            {
+                var response = await chronoskeysUrl
+                    .AppendPathSegment("api")
+                    .AppendPathSegment("PublicKey")
+                    .AppendPathSegment("byUser")
+                    .SetQueryParam("user", model.Username.ToString())
+                    .GetAsync();
+               
+                if (response.StatusCode.Equals(200))
+                {
+                    return false;
+                }
+            }
+            catch (FlurlHttpException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            return true;
+          }
+
+        [HttpGet]
+        public async Task<bool> PKDWakeUp()
+        {
+            try
+            {
+                var response = await chronoskeysUrl
+                    .GetAsync();
+
+                if (response.StatusCode.Equals(200))
+                {
+                    return true;
+                }
+            }
+            catch (FlurlHttpException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        [HttpPost]
+        public async Task<bool> PublishPublicKey(SignupViewModel model)
+        {
+            byte[] keyPair = Encryption.RSAService.RSAKeyGeneration();
+            byte[] publicKey = Encryption.RSAService.RSAPublicKeyOnly(keyPair);
+            String publicKeyStr = Encryption.RSAService.convertToString(publicKey);
+            //Debug.WriteLine("Public key: " + publicKeyStr);
+            var postParams = new KeyPost();
+            postParams.user = model.Username.ToString();
+            postParams.key = publicKeyStr;
+
+            var jsonString = JsonConvert.SerializeObject(postParams);
+            var content = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.PostAsync("https://chronoskeys.co/api/PublicKey", content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+
+
+            //try
+            //{
+            //    var responseMessage = await "https://chronoskeys.co/api/PublicKey".PostUrlEncodedAsync(new
+            //    {
+            //        user = model.Username.ToString(),
+            //        key = publicKeyStr
+            //    });
+            //    if (responseMessage.StatusCode.Equals(200))
+            //    {
+            //        return true;
+            //    }
+            //}
+            //catch (FlurlHttpException ex)
+            //{
+            //    Debug.WriteLine(ex.ToString());
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.ToString());
+            //}
+            return false;
+        }
+
+            
+        
+
         [HttpPost]
         public async Task<IActionResult> SignUp(SignupViewModel model)
         {
            
             if (ModelState.IsValid)
             {
-                try
+                bool userNameCheck = await CheckUsernameAvailability(model);
+                if (userNameCheck == true)
                 {
-                    HttpResponseMessage responseMessage = await "https://chronoschat.co/registration".PostUrlEncodedAsync(new
+                    bool publishKeyCheck = await PublishPublicKey(model);
+                    if (publishKeyCheck == true)
                     {
-                        email = model.Username.ToString(),
-                        password = model.Password.ToString(),
-                        first_name = model.FirstName,
-                        last_name = model.LastName
+                        try
+                        {
+                            HttpResponseMessage responseMessage = await "https://chronoschat.co/registration".PostUrlEncodedAsync(new
+                            {
+                                email = model.Username.ToString(),
+                                password = model.Password.ToString(),
+                                first_name = model.FirstName,
+                                last_name = model.LastName
 
-                    });
-                    if (responseMessage.IsSuccessStatusCode)
+                            });
+                            if (responseMessage.IsSuccessStatusCode)
+                            {
+                                return RedirectToAction("Login", "Auth");
+                            }
+                        }
+                        catch (FlurlHttpException ex)
+                        {
+                            Debug.WriteLine(ex.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.ToString());
+                        }
+                    }
+                    else
                     {
-                        return RedirectToAction("Login", "Auth");
+                        Debug.WriteLine("Public key did not post for " + model.Username + "." );
                     }
                 }
-                catch (FlurlHttpException ex)
+                else
                 {
-                    Debug.WriteLine(ex.ToString());
+                    Debug.WriteLine("Username is taken on chronoskeys.co.");
                 }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
+
             }
 
             return View();
